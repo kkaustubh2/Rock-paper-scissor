a4a017153820d692d596b847592b033d0ff70d3f
kkaustubh2 <723146521q@gmail.com>
2026-01-20 18:35:25 +0530

    first commit
    70% of the work is done. just gotta create a function and finish the
    work

---
+++ script.js
@@ -0,0 +1,32 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getPlayerChoice() {
+    let userEntry = parseInt(
+        prompt(`Enter number amongst 1(Rock), 2(Paper), 3(Scissor) `),
+    );
+    if (userEntry === 1) {
+        console.log("User chose rock");
+    } else if (userEntry === 2) {
+        console.log("User Chose paper");
+    } else if (userEntry === 3) {
+        console.log("User chose scissors");
+    } else {
+        console.log("Please enter a valid input");
+    }
+}
+
+function getComputerChoice() {
+    let computerEntry = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        console.log("Computer chose rock");
+    } else if (randomNumber === 1) {
+        console.log("Computer Chose Paper");
+    } else {
+        console.log("Computer Chose scissors");
+    }
+}
+
+function playRound(userEntry, computerEntry) {}
+getPlayerChoice();
+getComputerChoice();
